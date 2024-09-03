858b586f78f2c195302221a66f313c058c469a5b
WayZer <q1048203787@gmail.com>
2024-09-03 15:51:11 +0800

    fix

--- p4-bmv2/build.sh
+++ p4-bmv2/build.sh
@@ -1,7 +1,7 @@
 cd pi
 export BOOST_ROOT=$PREFIX/lib
 ./autogen.sh
-./configure --prefix=$PREFIX --with-proto --without-internal-rpc --without-cli --without-bmv2 [--with-sysrepo]
+./configure --prefix=$PREFIX --with-proto --without-internal-rpc --without-cli --without-bmv2
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
 cd ..
