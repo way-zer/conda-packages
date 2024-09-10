167725a70e4a1ff305d2bd4a33d4470f86588e9d
WayZer <q1048203787@gmail.com>
2024-09-10 17:50:42 +0800

    fix

--- p4-bmv2/build.sh
+++ p4-bmv2/build.sh
@@ -1,9 +1,10 @@
 cd pi
-git submodule update --init --recursive
+git status
+git restore proto third_party  # why conda modify files
 ./autogen.sh
-./configure --prefix=$PREFIX --with-proto --without-internal-rpc --without-cli --without-bmv2 --with-boost-libdir=$PREFIX/lib
+./configure --prefix=$PREFIX --with-proto --with-boost-libdir=$PREFIX/lib
 make -j${CPU_COUNT} ${VERBOSE_AT}
-make install
+make install-strip
 cd ..
 
 ./autogen.sh
