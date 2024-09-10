715a8483b900a8e654ea17c6faee36fb969a5c08
WayZer <q1048203787@gmail.com>
2024-09-10 17:40:38 +0800

    debug

--- p4-pi/build.sh
+++ p4-pi/build.sh
@@ -1,8 +1,6 @@
-
+git status
 ./autogen.sh
-set +e # don't know why, fail first time
-./configure --prefix=$PREFIX --with-proto --with-boost-libdir=$PREFIX/lib
-set -e
+git status
 ./configure --prefix=$PREFIX --with-proto --with-boost-libdir=$PREFIX/lib
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install-strip
