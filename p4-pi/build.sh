d2f53ac64f5e4bbc29688becdc8fd7bad42d3f0d
WayZer <q1048203787@gmail.com>
2024-09-10 17:45:47 +0800

    fix

--- p4-pi/build.sh
+++ p4-pi/build.sh
@@ -1,6 +1,6 @@
 git status
+git restore proto third_party  # why conda modify files
 ./autogen.sh
-git status
 ./configure --prefix=$PREFIX --with-proto --with-boost-libdir=$PREFIX/lib
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install-strip
