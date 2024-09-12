9eaf1dd4b378a14d99df7eb2a76ebc30c066da28
WayZer <user@wayzer.cf>
2024-09-12 06:18:22 +0000

    fix build, and remove python

--- p4lang-bmv2/build.sh
+++ p4lang-bmv2/build.sh
@@ -1,4 +1,8 @@
+sed -i 's/AM_PATH_PYTHON(\[3.5\],, \[:\])/AM_PATH_PYTHON()/g' configure.ac
+sed -i 's/AM_PATH_PYTHON(\[3.5\],, \[:\])/AM_PATH_PYTHON()/g' targets/simple_switch_grpc/configure.ac
 ./autogen.sh
+sed -i '/install.*PYTHON/d' */Makefile.in
+sed -i '/install.*PYTHON/d' targets/*/Makefile.in
 ./configure --prefix=$PREFIX --with-pi --without-nanomsg
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
--- p4lang-pi/build.sh
+++ p4lang-pi/build.sh
@@ -1,8 +1,10 @@
 git status
-git restore proto  # why rattler-build delete files
+git restore proto # why rattler-build delete files
 (cd third_party/uthash && git restore include)
 git status
+sed -i 's/AM_PATH_PYTHON(\[3.5\],, \[:\])/AM_PATH_PYTHON()/g' proto/configure.ac
 ./autogen.sh
+sed -i '/install.*PYTHON/d' proto/Makefile.in
 ./configure --prefix=$PREFIX --with-proto --without-internal-rpc --without-cli --without-bmv2 --with-boost-libdir=$PREFIX/lib
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install-strip
